87fb8de5572b15ca0fac7bc725a996888b5d400e
Sachin Chanduveetil <sachisavera@gmail.com>
2024-11-18 19:51:37 +0530

    Add files via upload

---
+++ Angular-GeminiAI/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from './customer/customer.component';
+import { AuthGuard } from './guard/auth.guard';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
+import { QaGenerativeComponent } from './qa-generative/qa-generative.component';
+
+const routes: Routes = [
+ {component:LoginComponent,path:'login'},
+ {component:RegisterComponent,path:'register'},
+ {component:HomeComponent,path:'',canActivate:[AuthGuard]},
+ {component:UserComponent,path:'user',canActivate:[AuthGuard]},
+ {component:CustomerComponent,path:'customer',canActivate:[AuthGuard]},
+ {component:QaGenerativeComponent, path: 'questioner'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
